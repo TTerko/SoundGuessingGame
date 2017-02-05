70016cddadf85f61a0355e71853cd3c63c53770c
Oleksandr Terletskyy <oles.terletskiy@gmail.com>
2017-02-05 02:00:36 +0200

    Add menu layout. Start implementing logic

---
+++ Assets/Scripts/Panels/GamePanelBehaviour.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Task
+{
+	public string OptionA;
+	public string OptionB;
+
+	public List<AudioClip> OptionAClips;
+	public List<AudioClip> OptionBClips;
+
+	public Task(string optionA, string optionB)
+	{
+		this.OptionA = optionA;
+		this.OptionB = optionB;
+
+		this.OptionAClips = new List<AudioClip>();
+		this.OptionBClips = new List<AudioClip>();
+	}
+
+	public void AddAClip(AudioClip audioClip)
+	{
+		OptionAClips.Add(audioClip);
+	}
+
+	public void AddBClip(AudioClip audioClip)
+	{
+		OptionBClips.Add(audioClip);
+	}
+
+}
+
+public class GamePanelBehaviour : GenericPanelBehaviour 
+{
+	public AudioClip AClip;
+	public AudioClip BClip;
+
+	private Task simpleTask;
+
+	void OnOpen()
+	{
+		simpleTask = new Task("DOG", "BOG");
+		simpleTask.AddAClip(AClip);
+		simpleTask.AddBClip(BClip);
+	}
+}
---
+++ Assets/Scripts/Panels/OptionsPanelBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsPanelBehaviour : GenericPanelBehaviour 
+{
+	void OnOpen()
+	{
+	
+	}
+
+	void OnClose()
+	{
+
+	}
+
+	public void OnOptionAClick()
+	{
+
+	}
+
+	public void OnOptionBClick()
+	{
+
+	}
+}
